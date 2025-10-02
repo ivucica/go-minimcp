6884ebe556e4853106f04d37a228cf24ac3d4b73
Ivan Vučica <ivan@vucica.net>
2025-10-02 20:14:40 +0100

    gofmt

--- cmd/minimcp/minimcp.go
+++ cmd/minimcp/minimcp.go
@@ -146,14 +146,14 @@ func main() {
 	switch *listenType {
 	case "stdio":
 		/*
-		In the stdio transport:
-		The client launches the MCP server as a subprocess.
-		The server reads JSON-RPC messages from its standard input (stdin) and sends messages to its standard output (stdout).
-		Messages are individual JSON-RPC requests, notifications, or responses.
-		Messages are delimited by newlines, and MUST NOT contain embedded newlines.
-		The server MAY write UTF-8 strings to its standard error (stderr) for logging purposes. Clients MAY capture, forward, or ignore this logging.
-		The server MUST NOT write anything to its stdout that is not a valid MCP message.
-		The client MUST NOT write anything to the server’s stdin that is not a valid MCP message.
+			In the stdio transport:
+			The client launches the MCP server as a subprocess.
+			The server reads JSON-RPC messages from its standard input (stdin) and sends messages to its standard output (stdout).
+			Messages are individual JSON-RPC requests, notifications, or responses.
+			Messages are delimited by newlines, and MUST NOT contain embedded newlines.
+			The server MAY write UTF-8 strings to its standard error (stderr) for logging purposes. Clients MAY capture, forward, or ignore this logging.
+			The server MUST NOT write anything to its stdout that is not a valid MCP message.
+			The client MUST NOT write anything to the server’s stdin that is not a valid MCP message.
 		*/
 		glog.Infof("using stdio rwc")
 		glog.Flush()
